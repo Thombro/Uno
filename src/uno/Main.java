02fa404798ef974375aa790bcbff93a2d59abcc5
PATNODEIA17 <PATNODEIA17@5CG73107TP.GCC.edu>
2018-11-30 11:30:42 -0500

    Fixed main

--- src/uno/Main.java
+++ src/uno/Main.java
@@ -1,16 +1,21 @@
 package uno;
 
+import java.util.Scanner;
+
 import javafx.application.Application;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 
 public class Main extends Application {
@@ -21,146 +26,150 @@ public class Main extends Application {
 	 */
 	Stage window;
 	Scene mainMenu;
-	Scene gameSetup;
 	Scene howToPlay;
 	Scene gamePlay;
+	private int numPlayers;
 	
 	public static void main(String[] args) {
-		launch(args);
+		boolean gui = false;
+		boolean incorrectEnter = true;
+		Scanner scnr = new Scanner(System.in);
+		while(incorrectEnter) {
+			System.out.println("Please enter c to play on the console or g to play on the graphic user interface.");
+			char choice = scnr.next().charAt(0);
+		
+			if(choice == 'G' || choice == 'g') {
+				gui = true;
+				incorrectEnter = false;
+			}
+			else if(choice == 'c' || choice == 'C') {
+				incorrectEnter = false;
+			}
+		}
+		
+		
+		if(gui) {
+			launch(args);
+		}
+		else {
+			Game newGame = new Game();
+			newGame.setup();
+		}
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Stage window = primaryStage;
 		
-//		Main Menu
-		Label gameName = new Label("UNO");
+		//Main Menu
+		
+		Text gameName = new Text("UNO");
+		gameName.setFont(Font.font("Serif", 100));
+		gameName.setFill(Color.YELLOW);
+		gameName.setStrokeWidth(1.5);
+		gameName.setStroke(Color.BLACK);
+		
 		Button startBtn = new Button("Start Game");
-		startBtn.setOnAction(e -> window.setScene(gameSetup));
+		startBtn.setOnAction(e -> window.setScene(gamePlay));
 		
 		Button ruleBtn = new Button("How to Play");
 		ruleBtn.setOnAction(e -> window.setScene(howToPlay));
 		
-		HBox layer1 = new HBox(20);
-		layer1.setAlignment(Pos.CENTER);
-		layer1.getChildren().add(gameName);
+		Text playText = new Text("Number of Players");
+		playText.setFont(Font.font("Serif", 15));
 		
-		HBox layer2 = new HBox(50);
-		layer2.setAlignment(Pos.CENTER);
-		layer2.getChildren().addAll(startBtn, ruleBtn);
+		final ToggleGroup groupOfPlayers = new ToggleGroup();
 		
-		VBox menuLayout = new VBox(50);
+		RadioButton play2 = new RadioButton("Two Player");
+		play2.setToggleGroup(groupOfPlayers);
+		
+		RadioButton play3 = new RadioButton("Three Player");
+		play3.setToggleGroup(groupOfPlayers);
+		
+		RadioButton play4 = new RadioButton("Four Player");
+		play4.setToggleGroup(groupOfPlayers);
+		
+		if(play2.isSelected()) {
+			numPlayers = 2;
+		}
+		else if(play3.isSelected()) {
+			numPlayers = 3;
+		}
+		else if(play4.isSelected()) {
+			numPlayers = 4;
+		}
+		
+		Game g = new Game(numPlayers);
+		
+		StackPane background = new StackPane();
+		background.setStyle("-fx-background-color: DARKRED;");
+		
+		HBox gameTitle = new HBox(20);
+		gameTitle.getChildren().add(gameName);
+		gameTitle.setAlignment(Pos.CENTER);
+		
+		HBox playerNumber = new HBox(40);
+		playerNumber.getChildren().addAll(play2, play3, play4);
+		playerNumber.setAlignment(Pos.CENTER);
+		
+		HBox buttons = new HBox(40);
+		buttons.getChildren().addAll(startBtn, ruleBtn);
+		buttons.setAlignment(Pos.CENTER);
+		
+		VBox menuLayout = new VBox(20);
+		menuLayout.getChildren().addAll(gameName, playText, playerNumber, buttons);
 		menuLayout.setAlignment(Pos.CENTER);
-		menuLayout.getChildren().addAll(layer1, layer2);
 		
-		mainMenu = new Scene(menuLayout, 500, 400);
+		background.getChildren().add(menuLayout);
 		
-//		How to Play
+		
+		mainMenu = new Scene(background, 600, 600);
+		
+		//How To Play 
+		
 		Button backBtn = new Button("Back");
 		backBtn.setOnAction(e -> window.setScene(mainMenu));
 		
 		Text rules =  new Text("UNO is a game where you try to empty your hand before your opponents.\n"
 				+ "Each player is dealt seven cards, the remaining cards forming the draw pile.\n"
 				+ "Afterwards, the top card of the draw pile is turned over to form the discard pile.\n" 
-				+ "The play consists of each player selecting a card from their hand by matching the\n" 
+				+ "The play consists of each player selecting a card from their hand by matching the " 
 				+ "color, number, or word of the top card of the discard pile. A wild card will always work.\n"
-				+ "If a player cannot play any of their cards, they must draw one card from the draw pile"
+				+ "If a player cannot play any of their cards, they must draw one card from the draw pile "
 				+ "if that card fits the sequence they may play it, otherwise their turn is over.\n"
-				+ "UNO has cards in four colors: red, blue, green, and yellow"
+				+ "UNO has cards in four colors: red, blue, green, and yellow.\n"
 				+ "It also has fifteen numbers and types: 0-9, wild, wild draw 4, skip, reverse, and draw 2.");
-
-		VBox ruleLayout = new VBox(20);
-		ruleLayout.getChildren().addAll(rules, backBtn);
+		rules.setFont(Font.font("Serif", 20));
+		rules.setWrappingWidth(500);
+		rules.setTextAlignment(TextAlignment.JUSTIFY);
 		
-		howToPlay = new Scene(ruleLayout, 600, 600);
+		StackPane htpBackground = new StackPane();
+		htpBackground.setStyle("-fx-background-color: LIGHTBLUE;");
 		
-//		Game Setup Menu
-		Group root = new Group();
-		ChoiceBox choiceBox = new ChoiceBox();
-		root.getChildren().add(choiceBox);
+		HBox back = new HBox(40);
+		back.getChildren().add(backBtn);
 		
-        choiceBox.getItems().add("Choice 1");
-        choiceBox.getItems().add("Choice 2");
-        choiceBox.getItems().add("Choice 3");
+		VBox ruleLayout = new VBox(50);
+		ruleLayout.getChildren().addAll(rules, back);
 		
-        String value = (String) choiceBox.getValue();
-        System.out.println(value);
-        
-        gameSetup = new Scene(root, 400, 400);
+		htpBackground.getChildren().add(ruleLayout);
+		
+		howToPlay = new Scene(htpBackground, 600, 600);
+		
+		//Game
+		
+		HBox gameLayout = new HBox(20);
+		
+		Button exit = new Button("Exit Game");
+		exit.setOnAction(e -> window.setScene(mainMenu));
+		
+		gameLayout.getChildren().addAll(exit);
+		
+		gamePlay = new Scene(gameLayout, 600, 600);
 		
 		window.setScene(mainMenu);
 		window.setTitle("UNO");
 		window.show();
 	
 	}	
-	
-	private void createMainMenu() {
-		Label gameName = new Label("UNO");
-		Button startBtn = new Button("Start Game");
-		startBtn.setOnAction(e -> window.setScene(gameSetup));
-		
-		Button ruleBtn = new Button("How to Play");
-		ruleBtn.setOnAction(e -> window.setScene(howToPlay));
-		
-		HBox layer1 = new HBox(20);
-		layer1.setAlignment(Pos.CENTER);
-		layer1.getChildren().add(gameName);
-		
-		HBox layer2 = new HBox(50);
-		layer2.setAlignment(Pos.CENTER);
-		layer2.getChildren().addAll(startBtn, ruleBtn);
-		
-		VBox menuLayout = new VBox(50);
-		menuLayout.setAlignment(Pos.CENTER);
-		menuLayout.getChildren().addAll(layer1, layer2);
-		
-		mainMenu = new Scene(menuLayout, 500, 400);
-	}
-	
-	
-	public void createHelpMenu() {
-		Button backBtn = new Button("Back");
-		backBtn.setOnAction(e -> window.setScene(mainMenu));
-		
-		Text rules =  new Text("UNO is a game where you try to empty your hand before your opponents.\n"
-				+ "Each player is dealt seven cards, the remaining cards forming the draw pile.\n"
-				+ "Afterwards, the top card of the draw pile is turned over to form the discard pile.\n" 
-				+ "The play consists of each player selecting a card from their hand by matching the\n" 
-				+ "color, number, or word of the top card of the discard pile. A wild card will always work.\n"
-				+ "If a player cannot play any of their cards, they must draw one card from the draw pile"
-				+ "if that card fits the sequence they may play it, otherwise their turn is over.\n"
-				+ "UNO has cards in four colors: red, blue, green, and yellow"
-				+ "It also has fifteen numbers and types: 0-9, wild, wild draw 4, skip, reverse, and draw 2.");
-
-		VBox ruleLayout = new VBox(20);
-		ruleLayout.getChildren().addAll(rules, backBtn);
-		
-		howToPlay = new Scene(ruleLayout, 600, 600);
-	}
-	
-	
-	public void createGameSetupMenu() {
-//		ListView pNumList = new ListView();
-//		
-//		pNumList.getItems().add("2");
-//		pNumList.getItems().add("3");
-//		pNumList.getItems().add("4");
-//		
-//		ObservableList selectedIndices = pNumList.getSelectionModel().getSelectedIndices();
-		
-		Group root = new Group();
-		ChoiceBox choiceBox = new ChoiceBox();
-		root.getChildren().add(choiceBox);
-		
-        choiceBox.getItems().add("Choice 1");
-        choiceBox.getItems().add("Choice 2");
-        choiceBox.getItems().add("Choice 3");
-		
-        String value = (String) choiceBox.getValue();
-        System.out.println(value);
-        
-        gameSetup = new Scene(root, 400, 400);
-		
-	}
-		
 }
